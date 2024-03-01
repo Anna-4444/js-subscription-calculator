75334e9694c8dba4f6d40b90f569abeabe5bb749
anna_b <nelson_anna9@hotmail.com>
2024-03-01 13:53:00 -0600

    Changed declared variables to const and let keyword from var keyword

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-// Your JS code here
-var subTypeElement = document.querySelector("#subscription");
-var subDurationElement = document.querySelector("#months");
-var result = document.querySelector(".result");
-var subType = "basic";
-var subDuration = 1;
+const subTypeElement = document.querySelector("#subscription");
+const subDurationElement = document.querySelector("#months");
+const result = document.querySelector(".result");
+let subType = "basic";
+let subDuration = 1;
 
 subTypeElement.addEventListener("change", function (e) {
     subType = e.target.value;
@@ -15,13 +14,13 @@ subDurationElement.addEventListener("change", function (e) {
     updateSubscriptionDiv ();
 })
 
-var updateSubscriptionDiv = function() {
-    var monthlyCost = 5;
+const updateSubscriptionDiv = function() {
+    let monthlyCost = 5;
     if (subType === "standard") {
         monthlyCost = 7
     } else if (subType === "premium") {
         monthlyCost = 10
     }
-    var total = (monthlyCost * subDuration).toFixed(2);
+    let total = (monthlyCost * subDuration).toFixed(2);
     result.innerText = `You have chosen the ${subDuration} month ${subType} plan for $${total}.`;
 };
